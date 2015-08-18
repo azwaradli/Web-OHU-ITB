df70a6148b842094a7416608c963bb57b09b6fc3
feryandi <feryandi.n@gmail.com>
2015-08-18 09:53:57 +0700

    Wordpress Theme

---
+++ wp-content/themes/ohuitb2015/footer.php
@@ -0,0 +1,9 @@
+
+    <?php wp_footer(); ?>
+	<script>
+	  new WOW().init();
+	  var s = skrollr.init();
+	</script>
+
+  </body>
+</html>
---
+++ wp-content/themes/ohuitb2015/index.php
@@ -0,0 +1,91 @@
+<?php get_header(); ?>
+<div class="wrapper">
+  <div class="container">    
+    <div class="main-content">
+      
+      <!-- Ubur ubur -->
+      <div class="cloud cloud-1" data-bottom-top="transform:translateY(0px)" data-top-bottom="transform:translateY(-400px)">
+        <img src="<?php bloginfo('template_url'); ?>/images/cloud-1.png">  
+      </div>
+      
+      <div class="cloud cloud-2" data-bottom-top="transform:translateY(0px)" data-top-bottom="transform:translateY(-400px)">
+        <img src="<?php bloginfo('template_url'); ?>/images/cloud-2.png">  
+      </div>
+      
+      <img src="<?php bloginfo('template_url'); ?>/images/logo.png" class="main-logo center-block" alt="Logo OHU ITB 2015">
+      <p>Selamat datang di pulau Nirbanadwipa Wonderland OHU. Tempat di mana semua mainan yang bisa kamu bayangkan menjadi nyata.</p>
+
+      <img src="<?php bloginfo('template_url'); ?>/images/nirbanadwipa.png" class="nirbanadwipa center-block" alt="Pulau Nirbanadwipa Wonderland OHU">
+      <p>Tiga pahlawan pulau Nirbanadwipa akan siap menyambutmu dan membuka matamu pada keajaiban masa kecil.</p>  
+    
+    </div>
+    
+    <div class="row">
+      
+      <div class="col-sm-4">
+        <img src="<?php bloginfo('template_url'); ?>/images/bhadrika.png" class="mascot-image center-block wow rotateIn" alt="Bhadrika">
+        <p class="mascot-name wow fadeInUp" data-wow-delay="100ms">Bhadrika</p>
+      </div>
+      
+      <div class="col-sm-4">
+        <img src="<?php bloginfo('template_url'); ?>/images/witamaya.png" class="mascot-image center-block wow rotateIn" data-wow-delay="400ms" alt="Witamaya">
+        <p class="mascot-name wow fadeInUp" data-wow-delay="500ms">Witamaya</p>
+      </div>
+      
+      <div class="col-sm-4">
+        <img src="<?php bloginfo('template_url'); ?>/images/ugraveda.png" class="mascot-image center-block wow rotateIn" data-wow-delay="800ms" alt="Ugraveda">
+        <p class="mascot-name wow fadeInUp" data-wow-delay="900ms">Ugraveda</p>
+      </div>
+    
+    </div>
+  
+  </div> <!-- End container -->
+  
+  <div class="timeline-ohu">
+  
+    <div class="container">
+      
+      <h2 class="timeline-ohu-title">Timeline OHU</h2>
+      
+      <p class="timeline-ohu-date">20 / 8</p>
+      <p class="timeline-ohu-event">defile ohu</p>
+      
+      <p class="timeline-ohu-date">25 / 8</p>
+      <p class="timeline-ohu-event">pre event</p>
+      
+      <p class="timeline-ohu-date"> 30 / 8</p>
+      <p class="timeline-ohu-event">OHU</p>
+
+    </div>
+  
+  </div>
+
+  <footer>
+
+    <div class="container">
+
+      <div class="row">
+        
+        <div class="col-sm-3 col-sm-offset-2">
+          <img src="<?php bloginfo('template_url'); ?>/images/footer-logo.png" class="footer-logo" alt="Logo OHU ITB 2015">
+        </div>  
+
+        <div class="col-sm-7 contact-wrapper">
+          <ul class="contact">
+            <li class="contact-askfm"><a href="http://ask.fm/OHUITB2015"></a></li>
+            <li class="contact-facebook"><a href="https://www.facebook.com/OHUITB2015"></a></li>
+            <li class="contact-instagram"><a href="https://instagram.com/ohuitb2015/"></a></li>
+            <li class="contact-line"><a href=""></a></li>
+            <li class="contact-twitter"><a href="https://twitter.com/ohuitb2015"></a></li>
+          </ul>
+        </div>
+
+      </div>
+      
+    </div>
+  
+  </footer>
+
+</div> <!-- End wrapper -->
+
+<?php get_footer(); ?>

fd315374c3823422a9f00aaa310dfb0c0e977bf4
feryandi <feryandi.n@gmail.com>
2015-08-18 15:12:12 +0700

    Updating Wordpress Theme

--- wp-content/themes/ohuitb2015/functions.php
+++ wp-content/themes/ohuitb2015/functions.php
@@ -37,5 +37,17 @@
 	}
 	add_action('init',register_theme_menus);
 
+	function create_widget( $name, $id, $description ) {
+		register_sidebar(array(
+			'name' => __( $name ),	 
+			'id' => $id, 
+			'description' => __( $description ),
+			'before_widget' => '<div class="widget">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3>',
+			'after_title' => '</h3>'
+		));
+	}
+	create_widget('Front Page Left', 'front-left', 'Display on the left of the homepage');
 
 ?>

8a5c6e25c20ddb4fe4b50e6ed007adb4ea7fba04
feryandi <feryandi.n@gmail.com>
2015-08-25 11:18:53 +0700

    Adding missing Rumpun Unit

---
+++ wp-content/themes/ohuitb2015/template-media.php
@@ -0,0 +1,54 @@
+<?php /* Template Name: Rumpun SeniBudaya */ get_header(); ?>
+    <div class="wrapper-unit">
+
+    <div class="mascot">
+      Unit Media
+    </div>
+
+      <div class="container-unit">
+
+          <!-- Ubur ubur -->
+          <div class="cloud cloud-1" data-bottom-top="transform:translateY(0px)" data-top-bottom="transform:translateY(-400px)">
+            <img src="<?php bloginfo('template_url');?>/images/cloud-1.png">  
+          </div>
+          
+          <div class="cloud cloud-2" data-bottom-top="transform:translateY(0px)" data-top-bottom="transform:translateY(-400px)">
+            <img src="<?php bloginfo('template_url');?>/images/cloud-2.png">  
+          </div>
+
+          <?php
+            global $post;
+            $args = array( 'posts_per_page' => 75, 'category_name' => 'seni-budaya' );
+            $posts = get_posts( $args );
+            foreach( $posts as $post ): setup_postdata($post); 
+          ?>
+
+          <div class="unit">
+            <div class="unit-name">
+              <?php echo the_title(); ?>
+            </div>
+            <div class="row">
+              <div class="col-md-3">
+                <?php echo the_post_thumbnail(); ?>
+              </div>
+              <div class="col-md-9">
+                <?php echo the_content(); ?>
+              </div>
+            </div>
+          </div>
+
+          <?php
+            endforeach; 
+          ?>
+
+      </div>
+
+      <div class="organigram">
+        
+        <div class="organigram-bg" data-bottom-top="transform: translateY(0px);" data-top-bottom="transform: translateY(50px);">
+        </div>
+        <img src="<?php bloginfo('template_url');?>/images/footer-page.png">
+
+      </div>
+
+<?php get_footer(); ?>
